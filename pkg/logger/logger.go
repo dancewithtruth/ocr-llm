3467453797eceeea9391cec9d3d5354d59d97cdf
Victor Wu <wuvictor95@gmail.com>
2023-05-05 10:42:06 -0700

    add logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+)
+
+type Logger interface {
+	With(args ...interface{}) Logger
+
+	WithoutCaller() Logger
+	Debug(args ...interface{})
+	// Info uses fmt.Sprint to construct and log a message at INFO level
+	Info(args ...interface{})
+	// Error uses fmt.Sprint to construct and log a message at ERROR level
+	Error(args ...interface{})
+
+	// Debugf uses fmt.Sprintf to construct and log a message at DEBUG level
+	Debugf(format string, args ...interface{})
+	// Infof uses fmt.Sprintf to construct and log a message at INFO level
+	Infof(format string, args ...interface{})
+	// Errorf uses fmt.Sprintf to construct and log a message at ERROR level
+	Errorf(format string, args ...interface{})
+}
+
+type logger struct {
+	*zap.SugaredLogger
+}
+
+// New creates a new logger using the default configuration
+func New() Logger {
+	l, _ := zap.NewProduction()
+	return NewSugar(l)
+}
+
+// NewSugar returns a SugaredLogger and implements the Logger interface
+func NewSugar(l *zap.Logger) Logger {
+	return &logger{l.Sugar()}
+}
+
+// With returns a logger based off the root logger and decorates it with the arguments
+func (l *logger) With(args ...interface{}) Logger {
+	return &logger{l.SugaredLogger.With(args...)}
+}
+
+// WithoutCaller returns a logger that does not output the caller field and location of the calling code.
+func (l *logger) WithoutCaller() Logger {
+	return &logger{l.SugaredLogger.WithOptions(zap.WithCaller(false))}
+}
